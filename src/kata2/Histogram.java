d3c80c49cc48f4c7403e4f300c101f6cad9fcaa4
NigelCubas1992 <NigelCubas1992@users.noreply.github.com>
2020-10-22 10:25:03 +0100

    project created

---
+++ src/kata2/Histogram.java
@@ -0,0 +1,34 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Nigel
+ */
+
+public class Histogram <T> {
+    
+    private final T[] data;
+
+    public Histogram(T[] data) {
+        this.data = data;
+    }
+
+    public T[] getData() {
+        return data;
+    }
+    
+    public Map<T,Integer> getHistogram(){
+        Map<T,Integer> histogram = new HashMap<T,Integer>();
+        for (T key : data) {
+            histogram.put(key,histogram.containsKey(key)?histogram.get(key)+1:1);
+        }
+        return histogram;
+    }
+    
+}
+
+
+
---
+++ src/kata2/Kata2.java
@@ -0,0 +1,30 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ *
+ * @author Nigel
+ */
+public class Kata2 {
+
+    public static void main(String[] args) {
+        String[] data = {"Pepe","Maria","Pedro","Juan","Pepe","Juan","Juan","Pedro","Pepe","Maria","Pedro","Pepe"}; 
+        
+        Histogram histo = new Histogram(data);
+        Map<String,Integer> histogram = histo.getHistogram(); 
+        
+        Iterator<Map.Entry<String,Integer>> entries= histogram.entrySet().iterator();
+        while(entries.hasNext()){
+            Map.Entry<String,Integer> entry = entries.next();
+            System.out.println("Key: "+ entry.getKey() + ", value: "+ entry.getValue());
+        }
+        
+    }
+    
+}
+
+
+
